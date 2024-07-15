80aad52c81a2d6711fddd6764c4e6f6d95147895
TRline11 <1985483049@qq.com>
2024-07-15 20:56:00 +0800

    左旋

---
+++ 左旋n个字符串/左旋n个字符串/左旋n个字符串.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void levorotation(char* arr,int n)
+{
+	int i = 0;
+	char* left=arr;
+	char* right =&arr[n-1];
+	while (left<right)
+	{
+		char temp = *left;
+		*left = *right;
+		*right = temp;
+		left++;
+		right--;
+	}
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int input = 0;
+	int middle = strlen(arr);
+	scanf("%d", &input);
+	levorotation(arr,input);
+	levorotation(arr + input, middle - input);
+	levorotation(arr, middle);
+	printf("%s\n", arr);
+	return 0;
+}
